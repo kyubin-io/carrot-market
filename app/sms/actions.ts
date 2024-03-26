cc10a45b872730e78f6f135526b41b1e216ca89b
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-03-26 22:18:41 +0900

    SMS validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,12 +1,45 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import validator from "validator";
 import { z } from "zod";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(validator.isMobilePhone, "Wrong phone format");
 
 const toekenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogin(prefState: any, formData: FormData) {
-  console.log(toekenSchema.parse(formData.get("token")));
+interface ActionState {
+  token: boolean;
+}
+
+export async function smsLogin(prevState: ActionState, formData: FormData) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return {
+        token: true,
+      };
+    }
+  } else {
+    const result = toekenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
 }
--- components/input.tsx
+++ components/input.tsx
@@ -13,6 +13,7 @@ export default function Input({
   return (
     <div className="flex flex-col gap-2">
       <input
+        key={name}
         name={name}
         className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-2 focus:ring-4 transition ring-neutral-200 focus:ring-orange-500 border-none placeholder:text-neutral-400"
         {...rest}
