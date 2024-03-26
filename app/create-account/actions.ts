47437d67098bf41115cb63aa61441697b278571e
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-03-26 16:24:04 +0900

    confirm password

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -1,12 +1,40 @@
 "use server";
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const passwordRegex = new RegExp(
+  /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).+$/
+);
+
+const checkPasswords = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string()
+      .min(3, "too short.")
+      .max(10, "too long.")
+      .toLowerCase()
+      .trim(),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(4)
+      .regex(
+        passwordRegex,
+        "A password must have lowercase, UPPERCASE, a number and special characters."
+      ),
+    confirm_password: z.string().min(4),
+  })
+  .refine(checkPasswords, {
+    message: "Both passwords should be the same.",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -18,5 +46,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = formSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
+  } else {
+    console.log(result.data);
   }
 }
