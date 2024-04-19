e70ab9fcb484e825538262c35958cb9fc9cad9c6
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-04-20 01:20:17 +0900

    password hashing

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -6,6 +6,7 @@ import {
 } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
+import bcrypt from "bcrypt";
 
 const checkPasswords = ({
   password,
@@ -74,5 +75,17 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     return result.error.flatten();
   } else {
+    const hashedPassword = await bcrypt.hash(result.data.password, 12);
+    const user = await db.user.create({
+      data: {
+        username: result.data.username,
+        email: result.data.email,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+      },
+    });
+    console.log(user);
   }
 }
