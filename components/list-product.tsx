5cee51bc8d09b9f5f0c784964100d9ad6aff631c
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-04-23 22:50:18 +0900

    add img

---
+++ components/list-product.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface ListProductProps {
+  title: string;
+  price: number;
+  created_at: Date;
+  photo: string;
+  id: number;
+}
+
+export default function ListProduct({
+  title,
+  price,
+  created_at,
+  photo,
+  id,
+}: ListProductProps) {
+  return (
+    <Link href={`/products/${id}`} className="">
+      <div>
+        {title}
+        <Image width={200} height={200} src={photo} alt={title} />
+      </div>
+    </Link>
+  );
+}
