eba0ce8f52ecb9ba6a316a1e0b865c79a8f496c0
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-04-23 22:13:35 +0900

    add skeleton

--- lib/session.ts
+++ lib/session.ts
@@ -7,7 +7,7 @@ interface SessionContent {
 
 export default function getSession() {
   return getIronSession<SessionContent>(cookies(), {
-    cookieName: "karrot",
+    cookieName: "Karrot",
     password: process.env.COOKIE_PASSWORD!,
   });
 }
