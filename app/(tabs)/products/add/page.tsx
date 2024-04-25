4eb418df936ad3b3d5a6b7313a3b5a1c7c1b5362
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-04-26 00:21:52 +0900

    product upload

--- app/(tabs)/products/add/page.tsx
+++ app/(tabs)/products/add/page.tsx
@@ -4,19 +4,43 @@ import Button from "@/components/button";
 import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
+import { uploadProduct } from "./actions";
+import { useFormState } from "react-dom";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
-  const onImageChange = () => {};
+  const onImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { files },
+    } = event;
+    if (!files) {
+      return;
+    }
+    const file = files[0];
+    const url = URL.createObjectURL(file);
+    setPreview(url);
+  };
+  const [state, action] = useFormState(uploadProduct, null);
+
   return (
     <div>
-      <form className="p-5 flex flex-col gap-5">
+      <form action={action} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
-          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer"
+          className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
+          style={{
+            backgroundImage: `url(${preview})`,
+          }}
         >
-          <PhotoIcon className="w-20" />
-          <div className="text-neutral-400 text-sm">Please add a picture.</div>
+          {preview === "" ? (
+            <>
+              <PhotoIcon className="w-20" />
+              <div className="text-neutral-400 text-sm">
+                Please add a picture.
+                {state?.fieldErrors.photo}
+              </div>
+            </>
+          ) : null}
         </label>
         <input
           onChange={onImageChange}
@@ -26,13 +50,26 @@ export default function AddProduct() {
           accept="image/*"
           className="hidden"
         />
-        <Input name="title" required placeholder="Title" type="text" />
-        <Input name="price" type="number" required placeholder="Price" />
+        <Input
+          name="title"
+          required
+          placeholder="제목"
+          type="text"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          name="price"
+          type="number"
+          required
+          placeholder="가격"
+          errors={state?.fieldErrors.price}
+        />
         <Input
           name="description"
           type="text"
           required
           placeholder="detail description"
+          errors={state?.fieldErrors.description}
         />
         <Button text="Submit" />
       </form>
