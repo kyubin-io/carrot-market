be24d9c01ec3685b48f627d3b6f8e55a2b118a82
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-04-28 22:32:35 +0900

    generateStaticParams

--- app/(tabs)/home/page.tsx
+++ app/(tabs)/home/page.tsx
@@ -31,7 +31,7 @@ export const metadata = {
   title: "Home",
 };
 
-export const revalidate = 60;
+// export const revalidate = 60;
 
 export default async function Products() {
   const initialProducts = await getInitialProducts();
